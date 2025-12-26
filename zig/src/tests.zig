de60aa57d038490b133affef5b661f6dde285c5e
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2025-12-26 23:51:11 +0100

    Cover ::make_sexp macro_shape

--- zig/src/tests.zig
+++ zig/src/tests.zig
@@ -1255,6 +1255,68 @@ test "ion 1.1 writer11 can encode macro_shape args (system $ion::make_struct sha
     try std.testing.expectEqualStrings("b", out_st.fields[1].name.text orelse return error.TestExpectedEqual);
 }
 
+test "ion 1.1 writer11 can encode macro_shape args (system $ion::make_sexp shape)" {
+    var arena = try ion.value.Arena.init(std.testing.allocator);
+    defer arena.deinit();
+
+    // Macro at address 1:
+    //   (macro m ($ion::make_sexp::x) (% x))
+    const body_sym_percent = try ion.value.makeSymbol(&arena, "%");
+    const body_sym_x = try ion.value.makeSymbol(&arena, "x");
+    const body_sx_items = arena.allocator().alloc(ion.value.Element, 2) catch return ion.IonError.OutOfMemory;
+    body_sx_items[0] = .{ .annotations = &.{}, .value = .{ .symbol = body_sym_percent } };
+    body_sx_items[1] = .{ .annotations = &.{}, .value = .{ .symbol = body_sym_x } };
+    const body_elem: ion.value.Element = .{ .annotations = &.{}, .value = .{ .sexp = body_sx_items } };
+
+    const shape: ion.macro.MacroShape = .{ .module = "$ion", .name = "make_sexp" };
+    const macro_params = [_]ion.macro.Param{.{ .ty = .macro_shape, .card = .one, .name = "x", .shape = shape }};
+    const macro_body = [_]ion.value.Element{body_elem};
+    const macro_defs = try std.testing.allocator.alloc(ion.macro.Macro, 2);
+    defer std.testing.allocator.free(macro_defs);
+    macro_defs[0] = .{ .name = null, .params = &.{}, .body = &.{} };
+    macro_defs[1] = .{ .name = "m", .params = @constCast(macro_params[0..]), .body = &macro_body };
+    const mactab: ion.macro.MacroTable = .{ .macros = macro_defs };
+
+    // Encode x as a sexp containing the macro shape's arguments: ([1,2] (3 4))
+    const list_items = arena.allocator().alloc(ion.value.Element, 2) catch return ion.IonError.OutOfMemory;
+    list_items[0] = .{ .annotations = &.{}, .value = .{ .int = .{ .small = 1 } } };
+    list_items[1] = .{ .annotations = &.{}, .value = .{ .int = .{ .small = 2 } } };
+    const list_elem: ion.value.Element = .{ .annotations = &.{}, .value = .{ .list = list_items } };
+
+    const sexp_items = arena.allocator().alloc(ion.value.Element, 2) catch return ion.IonError.OutOfMemory;
+    sexp_items[0] = .{ .annotations = &.{}, .value = .{ .int = .{ .small = 3 } } };
+    sexp_items[1] = .{ .annotations = &.{}, .value = .{ .int = .{ .small = 4 } } };
+    const sexp_elem: ion.value.Element = .{ .annotations = &.{}, .value = .{ .sexp = sexp_items } };
+
+    const shape_args_items = arena.allocator().alloc(ion.value.Element, 2) catch return ion.IonError.OutOfMemory;
+    shape_args_items[0] = list_elem;
+    shape_args_items[1] = sexp_elem;
+    const shape_arg_elem: ion.value.Element = .{ .annotations = &.{}, .value = .{ .sexp = shape_args_items } };
+    const args_by_param = [_][]const ion.value.Element{&.{shape_arg_elem}};
+
+    var out = std.ArrayListUnmanaged(u8){};
+    defer out.deinit(std.testing.allocator);
+    try out.appendSlice(std.testing.allocator, &.{ 0xE0, 0x01, 0x01, 0xEA });
+    try ion.writer11.writeMacroInvocationLengthPrefixedWithParams(
+        std.testing.allocator,
+        &out,
+        1,
+        macro_params[0..],
+        args_by_param[0..],
+        .{ .mactab = &mactab },
+    );
+
+    const elems = try ion.binary11.parseTopLevelWithMacroTable(&arena, out.items, &mactab);
+    try std.testing.expect(elems.len == 1);
+    try std.testing.expect(elems[0].value == .sexp);
+    const got = elems[0].value.sexp;
+    try std.testing.expectEqual(@as(usize, 4), got.len);
+    try std.testing.expectEqual(@as(i128, 1), got[0].value.int.small);
+    try std.testing.expectEqual(@as(i128, 2), got[1].value.int.small);
+    try std.testing.expectEqual(@as(i128, 3), got[2].value.int.small);
+    try std.testing.expectEqual(@as(i128, 4), got[3].value.int.small);
+}
+
 test "ion 1.1 writer11 can emit unqualified user macro invocation (non-length-prefixed)" {
     var arena = try ion.value.Arena.init(std.testing.allocator);
     defer arena.deinit();
